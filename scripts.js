997286059abcaf9b9e8a0d402f9684ff46af686e
Sececo <01sebas.co@gmail.com>
2025-09-14 00:46:20 -0500

    Adelanto, css y js

---
+++ scripts.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+let muro = document.getElementById('muro');
+let informacion = document.getElementById('info');
+let fotos = document.getElementById('fotos');
+let perfilUno =document.getElementById('main_section_perfil_id')
+let perfilDos =document.getElementById('main_section_perfil_id_dos')
+
+    let muroLink = document.getElementById('main_nav_muro');
+    let informacionLink = document.getElementById('main_nav_info');
+    let fotosLink = document.getElementById('main_nav_fotos');
+
+    function hideAllSections() {
+    muro.style.display = 'none';
+    informacion.style.display = 'none';
+    fotos.style.display = 'none';
+    perfilDos.style.display = 'none';
+    perfilUno.style.display = 'none';
+    
+    }
+    function showSection(a) {
+    a.style.display = 'flex';
+    }
+    hideAllSections();
+    showSection(info);
+    showSection(perfilDos);
+    muroLink.addEventListener("click", function() {
+        hideAllSections();
+        showSection(muro);
+        showSection(perfilUno);
+        console.log("click en muro");
+    });
+    informacionLink.addEventListener("click", function() {
+        hideAllSections();
+        showSection(informacion);
+        showSection(perfilDos);
+        console.log("click en información");
+    });
+    fotosLink.addEventListener("click", function() {
+        hideAllSections();
+        showSection(fotos);
+        showSection(perfilUno);
+        console.log("click en fotos");
+    });
+});
